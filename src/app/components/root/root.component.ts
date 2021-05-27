36af80b94e61040fd46db7c92fec05e451ad4dbc
AbstractCoder89 <alejandropenagos1998@gmail.com>
2021-05-27 01:15:58 -0500

    add node syncing dates

--- src/app/components/root/root.component.ts
+++ src/app/components/root/root.component.ts
@@ -249,6 +249,10 @@ export class RootComponent implements OnInit, OnDestroy {
         this.walletService.fetchBlockData();
     }
 
+    checkSyncDates() {
+        this.walletService.fetchBlockData();
+    }
+
     closeDetails(reason: string) {
         this.detailsService.hide();
     }
